8cdcc96af9e14c6d7c9e341938664e47d7f9ce70
Monilito Castro <mannycastro.epic@gmail.com>
2018-01-13 01:21:00 -0600

    TODO front end reports page

--- routes/survey.js
+++ routes/survey.js
@@ -1,11 +1,104 @@
+const Path = require('path-parser')
+const Url = require('url');
 const requireAuth = require('../services/requireauth');
 const requireCredits = require('../services/requirecredits');
 const Survey = require('../models/survey');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
+const _ = require('lodash');
+
+// TODO front end reports page
+
 module.exports = app =>{
-    app.get('/api/surveys/reply', (req, res)=>{
+    app.post('/api/surveys/webhook', (req,res)=>{
+        const wh = req.body;
+        // console.log(wh)
+        const value = _.chain(wh)
+            .map( (item)=>{
+                const { url, email } = item;
+                const path = new Path('/api/:surveyId/:answer');
+                const { pathname } = Url.parse(url);
+                const urlParams = path.test(pathname);
+                const {surveyId, answer} = urlParams;
+                return {email, surveyId, answer};
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each( (item)=>{
+                const { email, surveyId, answer } = item;
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch:{
+                                email : email,
+                                responded: false
+                            }
+                        }
+                    },
+                    {
+                        $set: {
+                            "recipients.$.responded": true,
+                            "lastResponded" : Date.now()
+                        },
+                        $inc: {
+                            [answer]: 1
+                        }
+                    }
+                )
+                .exec();
+    
+            })
+            .value();
+        // console.log('VALUE', value);
+
+
+        res.status(200).send('OK');
+    })
+    app.post('/api/surveys/webhook_OLD', (req,res)=>{
+        const wh = req.body;
+        // console.log(wh)
+        const value = _.chain(wh)
+            .compact()
+            .uniqBy('email', 'sg_event_id')
+            .each( (item)=>{
+                const { email, url } = item;
+                const path = new Path('/api/:surveyId/:answer');
+                const { pathname } = Url.parse(url);
+                const urlParams = path.test(pathname);
+                const {surveyId, answer} = urlParams;
+                console.log('OUTPUT:', {email, surveyId, answer})
+                // TODO sql injection protect 'answer'
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch:{
+                                "email" : email,
+                                "responded": false
+                            }
+                        }
+                    },
+                    {
+                        $set: {
+                            "recipients.$.responded": true,
+                            "lastResponded" : Date.now()
+                        },
+                        $inc: {
+                            [answer]: 1
+                        }
+                    }
+                )
+    
+            })
+            .value();
+        // console.log('VALUE', value);
+
+
+        res.status(200).send('OK');
+    })
+    app.get('/api/:surveys/:reply', (req, res)=>{
         res.setHeader('content-type', 'text/html')
         res.end('<h1 style="text-align:center; margin-top:20px"> Thank you :) </h1>');
     })
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -5,8 +5,8 @@ module.exports = (survey) => {
             <h1>${survey.title}</h1>
             <p>${survey.body}</p>
             <div style='text-align: center;'>
-                <a href='${keys.redirectURL}/api/surveys/reply'>Yes</a>
-                <a href='${keys.redirectURL}/api/surveys/reply'>No</a>
+                <a href='${keys.redirectURL}/api/${survey.id}/yes'>Yes</a>
+                <a href='${keys.redirectURL}/api/${survey.id}/no'>No</a>
             </div>
         </div>
     `
